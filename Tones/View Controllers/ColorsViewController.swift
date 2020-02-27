a2c501b697e4af9551f1d513df453b4e850f51cb
Nicolas G. Querol <nicolas.gquerol@gmail.com>
2020-02-28 00:37:06 +0100

    Move colors datasource to its own struct

--- Tones/View Controllers/ColorsViewController.swift
+++ Tones/View Controllers/ColorsViewController.swift
@@ -16,6 +16,10 @@ class ColorsViewController: NSViewController {
     @IBOutlet var appearanceSegmentedControl: NSSegmentedControl! {
         didSet {
             appearanceSegmentedControl.segmentCount = supportedAppearances.count
+
+            for i in 0 ..< appearanceSegmentedControl.segmentCount {
+                appearanceSegmentedControl.setWidth(0, forSegment: i)
+            }
         }
     }
 
@@ -27,119 +31,7 @@ class ColorsViewController: NSViewController {
 
     private let supportedAppearances: [NSAppearance.Name] = [.aqua, .darkAqua]
 
-    private let appearanceFriendlyNames: [NSAppearance.Name: String] = [.aqua: "Light", .darkAqua: "Dark"]
-
-    private let sectionNames = [
-        "System",
-        "Fixed",
-        "Labels",
-        "Text",
-        "Content",
-        "Menus",
-        "Tables",
-        "Controls",
-        "Windows",
-        "Highlights and Shadows",
-        "Deprecated",
-    ]
-
-    private let colors: [[(NSColor, String)]] = [
-        [
-            (.systemBlue, "systemBlue"),
-            (.systemBrown, "systemBrown"),
-            (.systemGray, "systemGray"),
-            (.systemGreen, "systemGreen"),
-            (.systemOrange, "systemOrange"),
-            (.systemPink, "systemPink"),
-            (.systemPurple, "systemPurple"),
-            (.systemRed, "systemRed"),
-            (.systemYellow, "systemYellow"),
-        ],
-        [
-            (.black, "black"),
-            (.blue, "blue"),
-            (.brown, "brown"),
-            (.cyan, "cyan"),
-            (.darkGray, "darkGray"),
-            (.gray, "gray"),
-            (.green, "green"),
-            (.lightGray, "lightGray"),
-            (.magenta, "magenta"),
-            (.orange, "orange"),
-            (.purple, "purple"),
-            (.red, "red"),
-            (.white, "white"),
-            (.yellow, "yellow"),
-        ],
-        [
-            (.labelColor, "labelColor"),
-            (.secondaryLabelColor, "secondaryLabelColor"),
-            (.tertiaryLabelColor, "tertiaryLabelColor"),
-            (.quaternaryLabelColor, "quaternaryLabelColor"),
-        ],
-        [
-            (.textColor, "textColor"),
-            (.placeholderTextColor, "placeholderTextColor"),
-            (.selectedTextColor, "selectedTextColor"),
-            (.textBackgroundColor, "textBackgroundColor"),
-            (.selectedTextBackgroundColor, "selectedTextBackgroundColor"),
-            (.keyboardFocusIndicatorColor, "keyboardFocusIndicatorColor"),
-            (.unemphasizedSelectedTextColor, "unemphasizedSelectedTextColor"),
-            (.unemphasizedSelectedTextBackgroundColor, "unemphasizedSelectedTextBackgroundColor"),
-        ],
-        [
-            (.linkColor, "linkColor"),
-            (.separatorColor, "separatorColor"),
-            (.selectedContentBackgroundColor, "selectedContentBackgroundColor"),
-            (.unemphasizedSelectedContentBackgroundColor, "unemphasizedSelectedContentBackgroundColor"),
-        ],
-        [
-            (.selectedMenuItemTextColor, "selectedMenuItemTextColor"),
-        ],
-        [
-            (.gridColor, "gridColor"),
-            (.headerTextColor, "headerTextColor"),
-            (NSColor.alternatingContentBackgroundColors[0], "alternatingContentBackgroundColors (even)"),
-            (NSColor.alternatingContentBackgroundColors[1], "alternatingContentBackgroundColors (odd)"),
-        ],
-        [
-            (.controlAccentColor, "controlAccentColor"),
-            (.controlColor, "controlColor"),
-            (.controlBackgroundColor, "controlBackgroundColor"),
-            (.controlTextColor, "controlTextColor"),
-            (.disabledControlTextColor, "disabledControlTextColor"),
-            (.selectedControlColor, "selectedControlColor"),
-            (.selectedControlTextColor, "selectedControlTextColor"),
-            (.alternateSelectedControlTextColor, "alternateSelectedControlTextColor"),
-            (.scrubberTexturedBackground, "scrubberTexturedBackground"),
-        ],
-        [
-            (.windowBackgroundColor, "windowBackgroundColor"),
-            (.windowFrameTextColor, "windowFrameTextColor"),
-            (.underPageBackgroundColor, "underPageBackgroundColor"),
-        ],
-        [
-            (.findHighlightColor, "findHighlightColor"),
-            (.highlightColor, "highlightColor"),
-            (.shadowColor, "shadowColor"),
-        ],
-        [
-            (.alternateSelectedControlColor, "alternateSelectedControlColor"),
-            (NSColor.controlAlternatingRowBackgroundColors[0], "controlAlternatingRowBackgroundColors (even)"),
-            (NSColor.controlAlternatingRowBackgroundColors[1], "controlAlternatingRowBackgroundColors (odd)"),
-            (.controlHighlightColor, "controlHighlightColor"),
-            (.controlLightHighlightColor, "controlLightHighlightColor"),
-            (.controlShadowColor, "controlShadowColor"),
-            (.controlDarkShadowColor, "controlDarkShadowColor"),
-            (.headerColor, "headerColor"),
-            (.knobColor, "knobColor"),
-            (.selectedKnobColor, "selectedKnobColor"),
-            (.scrollBarColor, "scrollBarColor"),
-            (.secondarySelectedControlColor, "secondarySelectedControlColor"),
-            (.selectedMenuItemColor, "selectedMenuItemColor"),
-            (.windowFrameColor, "windowFrameColor"),
-        ],
-    ]
+    private let friendlyAppearanceNames: [NSAppearance.Name: String] = [.aqua: "Light", .darkAqua: "Dark"]
 
     private var keyValueObservation: NSKeyValueObservation?
 
@@ -190,13 +82,11 @@ class ColorsViewController: NSViewController {
             let appearance = supportedAppearances[i]
 
             if appearance == systemAppearance {
-                appearanceSegmentedControl.setLabel("\(appearanceFriendlyNames[appearance, default: appearance.rawValue]) (System)", forSegment: i)
+                appearanceSegmentedControl.setLabel("\(friendlyAppearanceNames[appearance, default: appearance.rawValue]) (System)", forSegment: i)
                 appearanceSegmentedControl.selectedSegment = i
             } else {
-                appearanceSegmentedControl.setLabel(appearanceFriendlyNames[appearance, default: appearance.rawValue], forSegment: i)
+                appearanceSegmentedControl.setLabel(friendlyAppearanceNames[appearance, default: appearance.rawValue], forSegment: i)
             }
-
-            appearanceSegmentedControl.setWidth(0, forSegment: i)
         }
     }
 }
@@ -205,33 +95,39 @@ class ColorsViewController: NSViewController {
 
 extension ColorsViewController: NSCollectionViewDataSource {
     func numberOfSections(in _: NSCollectionView) -> Int {
-        colors.count
+        ColorDataSource.ColorSection.allCases.count
     }
 
     func collectionView(_: NSCollectionView, numberOfItemsInSection section: Int) -> Int {
-        return colors[section].count
+        ColorDataSource[section].count
     }
 
     func collectionView(_ collectionView: NSCollectionView, itemForRepresentedObjectAt indexPath: IndexPath) -> NSCollectionViewItem {
         let item = collectionView.makeItem(withIdentifier: .colorCollectionViewItem, for: indexPath)
 
         guard let collectionViewItem = item as? ColorCollectionViewItem else { return item }
-        
-        let color = colors[indexPath.section][indexPath.item]
 
-        collectionViewItem.colorView.representedColor = color.0
-        collectionViewItem.colorLabel.stringValue = color.1
+        let (color, name) = ColorDataSource[indexPath]
+
+        collectionViewItem.colorView.representedColor = color
+        collectionViewItem.colorLabel.stringValue = name
 
         return collectionViewItem
     }
 
-    func collectionView(_ collectionView: NSCollectionView, viewForSupplementaryElementOfKind _: String, at indexPath: IndexPath) -> NSView {
-        let view = collectionView.makeSupplementaryView(ofKind: NSCollectionView.elementKindSectionHeader, withIdentifier: .sectionHeaderView, for: indexPath)
+    func collectionView(_ collectionView: NSCollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> NSView {
+        let view = collectionView.makeSupplementaryView(
+            ofKind: kind,
+            withIdentifier: .sectionHeaderView,
+            for: indexPath
+        )
 
         guard let sectionHeaderView = view as? SectionHeaderView else { return view }
 
-        sectionHeaderView.sectionTitleLabel.stringValue = sectionNames[indexPath.section]
-        sectionHeaderView.sectionItemCountLabel.stringValue = "\(colors[indexPath.section].count) colors"
+        let category = ColorDataSource.ColorSection.allCases[indexPath.section]
+
+        sectionHeaderView.sectionTitleLabel.stringValue = category.description
+        sectionHeaderView.sectionItemCountLabel.stringValue = "\(category.colors.count) color(s)"
 
         return sectionHeaderView
     }
